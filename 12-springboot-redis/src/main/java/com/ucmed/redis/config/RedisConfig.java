97111b46421c69df9fa2d957c60fec76bd334bc7
aniver <2634817136@qq.com>
2020-02-27 14:55:40 +0800

    新增RedisUtil工具类

--- 12-springboot-redis/src/main/java/com/ucmed/redis/config/RedisConfig.java
+++ 12-springboot-redis/src/main/java/com/ucmed/redis/config/RedisConfig.java
@@ -1,46 +1,69 @@
-package com.ucmed.redis.config;
-
-import com.ucmed.redis.model.User;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
-
-/**
- *
- */
-@Configuration
-public class RedisConfig {
-
+//package com.ucmed.redis.config;
+//
+//import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+//import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+//import com.ucmed.redis.model.User;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+//import org.springframework.data.redis.serializer.StringRedisSerializer;
+//
+///**
+// *
+// */
+//@Configuration
+//public class RedisConfig {
+//
+//
 //    /**
-//        自定义Redis序列化器
+//     * 自定义Redis序列化器(jackson)
 //     */
 //    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
-//        RedisTemplate<String, Object> template = new RedisTemplate<>();
-//        // 关联
+//    public RedisTemplate<Object, Object> fastRedisTemplate(RedisConnectionFactory factory) {
+//        RedisTemplate<Object, Object> template = new RedisTemplate<>();
 //        template.setConnectionFactory(factory);
-//        // 设置key的序列化器
+//        FastJsonRedisSerializer<Object> serializer = new FastJsonRedisSerializer<>(Object.class);
+//
+//        // key的序列化采用StringRedisSerializer
 //        template.setKeySerializer(new StringRedisSerializer());
-//        // 设置value的序列化器
-//        template.setValueSerializer(new StringRedisSerializer());
+//        template.setHashKeySerializer(new StringRedisSerializer());
+//
+//        // value值的序列化采用fastJsonRedisSerializer
+//        template.setHashValueSerializer(serializer);
+//        template.setValueSerializer(serializer);
+//        return template;
+//    }
+//
+//
+////    /**
+////     *   自定义Redis序列化器(jackson)
+////     */
+////    @Bean
+////    public RedisTemplate<String, Object> objectRedisTemplate(RedisConnectionFactory factory) {
+////        RedisTemplate<String, Object> template = new RedisTemplate<>();
+////        // 关联
+////        template.setConnectionFactory(factory);
+////        // 设置key的序列化器
+////        template.setKeySerializer(new StringRedisSerializer());
+////        // 设置value的序列化器
+////        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(Object.class));
+////
+////        return template;
+////    }
+//
+//    /**
+//     * 配置USER的自定义序列化器
+//     */
+//    @Bean
+//    public RedisTemplate<String, User> userRedisTemplate(RedisConnectionFactory factory) {
+//
+//        RedisTemplate<String, User> template = new RedisTemplate<>();
+//        template.setConnectionFactory(factory);
+//        template.setKeySerializer(new StringRedisSerializer());
+//        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(User.class));
 //
 //        return template;
 //    }
-
-    /**
-     * 配置USER的自定义序列化器
-     */
-    @Bean
-    public RedisTemplate<String, User> userRedisTemplate(RedisConnectionFactory factory) {
-
-        RedisTemplate<String, User> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
-        template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new Jackson2JsonRedisSerializer<>(User.class));
-
-        return template;
-    }
-}
+//}
--- 12-springboot-redis/src/test/java/com/ucmed/redis/RedisTest.java
+++ 12-springboot-redis/src/test/java/com/ucmed/redis/RedisTest.java
@@ -1,6 +1,8 @@
 package com.ucmed.redis;
 
+import com.alibaba.fastjson.JSONObject;
 import com.ucmed.redis.model.User;
+import com.ucmed.redis.utils.RedisUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,22 +11,55 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class RedisTest {
-    @Autowired
-    private RedisTemplate<String, String> redisTemplate;
+//    @Autowired
+//    private RedisTemplate<String, Object> objectRedisTemplate;
+//
+//    @Autowired
+//    private RedisTemplate<Object, Object> fastRedisTemplate;
+//
+//    @Autowired
+//    private RedisTemplate<String, User> userRedisTemplate;
 
     @Autowired
-    private RedisTemplate<String, User> userRedisTemplate;
+    private RedisUtils redisUtils;
 
     private User user;
 
+    private List<User> users;
+
     @Test
     public void set() {
-        redisTemplate.opsForValue().set("myKey", "myValue");
+//        objectRedisTemplate.opsForValue().set("user", user);
+//        System.out.println(objectRedisTemplate.opsForValue().get("user"));
+//
+//        // 存储list
+//        objectRedisTemplate.opsForValue().set("users", users);
+//        System.out.println(objectRedisTemplate.opsForValue().get("users"));
 
-        System.out.println(redisTemplate.opsForValue().get("myKey"));
+//        fastRedisTemplate.opsForValue().set("class", "三年二班");
+//        System.out.println(fastRedisTemplate.opsForValue().get("class"));
+
+        // 设置key-value
+        redisUtils.set("a", "a");
+        // 获取value
+        redisUtils.get("a");
+        // 删除key
+        redisUtils.delete("a");
+        // 判断是否含有相应的key
+        redisUtils.hasKey("a");
+
+        // 设置key-vlue,并设置过期时间
+        redisUtils.setEx("user", JSONObject.toJSONString(user), 5, TimeUnit.SECONDS);
+        System.out.println(redisUtils.get("user"));
+        //redisUtils.delete("user");
+
     }
 
     @Before
@@ -35,11 +70,13 @@ public class RedisTest {
         user.setUsername("李四");
         user.setPassword("123456");
         user.setEmail("123@123.com");
+        users = new ArrayList<>();
+        users.add(user);
     }
 
     @Test
     public void tsetSet() {
-        userRedisTemplate.opsForValue().set("user", user);
-        System.out.println(userRedisTemplate.opsForValue().get("user"));
+//        userRedisTemplate.opsForValue().set("user", user);
+//        System.out.println(userRedisTemplate.opsForValue().get("user"));
     }
 }
