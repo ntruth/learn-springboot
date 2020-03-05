faf592791af86ec0469d51ecb446fb71bd8b65c9
aniver <2634817136@qq.com>
2020-03-05 18:00:30 +0800

    新增elasticsearch模块

---
+++ 18-springboot-elasticsearch/spring-data-jest/src/main/java/com/ucmed/elasticsearch/ElasticsearchApplication.java
@@ -0,0 +1,15 @@
+package com.ucmed.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+
+@SpringBootApplication(exclude = {ElasticsearchAutoConfiguration.class, ElasticsearchDataAutoConfiguration.class})
+public class ElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class, args);
+    }
+
+}
